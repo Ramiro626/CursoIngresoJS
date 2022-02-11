1ab1283dfbde540769fe0484ff8c91b2c310df48
Ramiro626 <rama626777@gmail.com>
2022-02-11 10:38:06 -0800

    TP Clase 5

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,4 +1,7 @@
-/*4.	Para el departamento de iluminación:
+/*4.
+Ramiro Sanchez DIV E
+TP Ejercicio 4
+Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
@@ -10,5 +13,85 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+ 	//declaro variables
+	    let lamparas;
+        let precio;
+        let descuento;
+        let impuesto;
+        var local;
+
+
+	//inicializo variables 
+		lamparas = document.getElementById("txtIdCantidad").value;
+		local = document.getElementById("Marca").value;
+
+	//convertir a Integer
+		lamparas = parseInt(lamparas);
+
+	//calculo descuento
+	if (lamparas >= 6) 
+	    {
+            descuento = 0.5;
+        }
+    else if (lamparas == 5)
+        {
+            if (local == "ArgentinaLuz")
+                {
+                    descuento = 0.6;
+                }
+            else
+                {
+                    descuento = 0.7;
+                }
+        }
+    else if (lamparas == 4)
+        {
+            if (local == "ArgentinaLuz" || local == "FelipeLamparas")
+                {
+                    descuento = 0.75;
+                }
+                else 
+                {
+                    descuento = 0.8;
+                }
+        }
+    else if (lamparas == 3)
+        {
+            if(local == "ArgentinaLuz")
+                {
+                    descuento = 0.85;
+                }
+            else if(local == "FelipeLamparas")
+                {
+                    descuento = 0.9;
+                }
+            else
+                {
+                    descuento = 0.95;
+                }
+        }
+       else 
+        {
+            descuento = 1;
+        }
+
+    //calculo precio, agrego ingreso bruto si necesario y display por document
+        precio = (lamparas * 35) * descuento;
+        
+        if(precio > 120)
+            {
+                impuesto = precio / 100 * 10;
+                precio = impuesto + precio;
+                document.getElementById("txtIdprecioDescuento").value = precio;
+                alert("IIBB Usted pago " + impuesto);
+            }
+        else
+            {
+                document.getElementById("txtIdprecioDescuento").value = precio;
+            }
+    
 }
+
+//txtIdCantidad
+//Marca
+//txtIdprecioDescuento
