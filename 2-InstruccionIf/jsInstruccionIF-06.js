e0edb157530a5b44198e7405bb49ca1493dce23d
Ramiro626 <rama626777@gmail.com>
2022-02-14 09:43:09 -0800

    Ejercicio BIS 6

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,7 +1,7 @@
 /*
 Ramiro Sanchez DIV E
 IF Ejercicio 6
-*/
+
 function mostrar()
 {
 	//declaro variables
@@ -29,3 +29,58 @@ function mostrar()
 }//FIN DE LA FUNCIÓN
 
 //txtIdEdad
+*/
+
+/*
+Ramiro Sanchez DIV E
+IF Ejercicio 6 BIS
+
+Ejercicio 6 bis(usar if else):
+Al ingresar por prompt una edad debemos informar si
+la persona es anciano(mas de 70), mayor de edad (entre 70 y 18 años),
+adolescente (entre 13 y 17 años), niño (menor a 13 años), bebe (0 a 3 años).
+mostrar por ID el resultado.
+*/
+
+function mostrar()
+{
+	//declaro variables
+		let edad;
+
+	//tomo la edad  
+		edad = prompt("¿Que edad tienes?");
+
+	//convertir a Integer
+		edad = parseInt(edad);
+
+	//display por alert con if
+		if(edad>70)
+		{
+			alert("usted es un anciano");
+		}
+		else 
+		{
+			if(edad>17 && edad<71)
+			{
+				alert("usted es mayor de edad");
+			}
+			else 
+			{
+				if(edad>12 && edad<18)
+				{
+					alert("usted es un adolescente");
+				}
+				else 
+				{
+					if (edad>3 && edad<13)
+					{
+						alert("usted es un niño");
+					}
+					else
+					{
+						alert("usted es un bebe");
+					}
+				}
+			}
+		}
+}//FIN DE LA FUNCIÓN
